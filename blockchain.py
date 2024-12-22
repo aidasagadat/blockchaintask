bbfec2bf2280c85282ef56c076c159bbff451cb9
aidasagadat <aidasagadat355@gmail.com>
2024-12-22 22:16:06 +0500

    Define Block class with attributes for previous hash, timestamp, transactions, Merkle root, and block hash.

--- blockchain.py
+++ blockchain.py
@@ -80,3 +80,12 @@ def create_merkle_root(transactions):
     while len(tx_hashes) > 1:
         tx_hashes = [sha256((tx_hashes[i] + tx_hashes[i+1]).encode('utf-8')) for i in range(0, len(tx_hashes), 2)]
     return tx_hashes[0]
+
+
+class Block:
+    def __init__(self, previous_hash, timestamp, transactions):
+        self.previous_hash = previous_hash
+        self.timestamp = timestamp
+        self.transactions = transactions
+        self.merkle_root = create_merkle_root(transactions)
+        self.hash = self.hash_block()
